d6b9d85059a7bb93954801b24fb88b5ed27c18ad
Euan Scott-Watson <37578825+EuanScottWatson@users.noreply.github.com>
2021-12-23 10:33:07 +0000

    Done multiple ants and started introducing pheremones

--- simulation.py
+++ simulation.py
@@ -6,20 +6,27 @@ import numpy as np
 
 
 class ACO:
-    def __init__(self, w, h):
-        # self.points = [(w * random(), h * random()) for _ in range(10)]
-        self.points = [(563.393724661267, 288.75083844227373), (345.92947657945706, 176.83411403693992), (381.3848691179929, 413.1509190241147), (155.71624127162113, 143.99370393070978), (516.3181894639458, 634.4476353193571), (622.9478205975814, 2.4471244449268568), (619.2984836361079, 217.09557804709794), (401.83421393343883, 172.67997267144972), (117.79096238078942, 148.79478942445724), (230.06685631248374, 23.860267697892812)]
+    def __init__(self, w, h, ants=1, points=25):
+        self.ants = ants
+        self.distances = [0 for _ in range(ants)]
+        self.points = [(w * random(), h * random()) for _ in range(points)]
+        self.pheremones = [[0 for _ in range(points)] for _ in range(points)]
+        print(self.points)
 
-        self.path = [choice(self.points)]
-        self.to_visit = deepcopy(self.points)
-        self.to_visit.remove(self.path[0])
+        # self.points = [(563.393724661267, 288.75083844227373), (345.92947657945706, 176.83411403693992), (381.3848691179929, 413.1509190241147), (155.71624127162113, 143.99370393070978), (516.3181894639458, 634.4476353193571), (622.9478205975814, 2.4471244449268568), (619.2984836361079, 217.09557804709794), (401.83421393343883, 172.67997267144972), (117.79096238078942, 148.79478942445724), (230.06685631248374, 23.860267697892812)]
+
+        self.path = [[choice(self.points)] for _ in range(ants)]
+        self.to_visit = [deepcopy(self.points) for _ in range(ants)]
+        for a in range(ants):
+            self.to_visit[a].remove(self.path[a][0])
 
     def display(self, screen):
         for p in self.points:
             pygame.draw.circle(screen, (0, 0, 0), (p[0], p[1]), 5, width=2)
         
-        if len(self.path) > 2:
-            pygame.draw.lines(screen, (0, 0, 0), False, self.path, 2)
+        for i, path in enumerate(self.path):
+            if len(path) > 2 and (i == np.argmin(self.distances) or not all(self.distances)):
+                pygame.draw.lines(screen, (0, 0, 0), False, path, 2)
 
     def events(self):
         for event in pygame.event.get():
@@ -38,23 +45,42 @@ class ACO:
         pygame.display.flip()
 
     def run_logic(self):
-        self.traverse()
+        if not all(self.distances):
+            for a in range(self.ants):
+                self.traverse(a)
+        else:
+            print("Ant no. {0} found a distance of {1} km".format(np.argmin(self.distances), round(min(self.distances), 2)))
+            self.generate_pheremones()
+
+    def generate_pheremones(self):
+        pass
 
     def generate_roulette(self, curr, remaining):
-        weights = list(map(lambda x: 1 / np.linalg.norm(np.array(x) - np.array(curr)), remaining))
+        dist_weights = list(map(lambda x: 1 / self.distance(x, curr), remaining))
+        dist_norm = [float(x) / max(dist_weights) for x in dist_weights]
+
+        pheremone_weights = list(map(lambda x: self.pheremones[self.points.index(curr)][self.points.index(x)], remaining))
+
+        weights = [a + b for (a, b) in zip(dist_norm, pheremone_weights)]
+
         total = sum(weights)
         probabilities = list(map(lambda x: x / total, weights))
+
         return probabilities
 
+    def distance(self, a, b):
+        return np.linalg.norm(np.array(a) - np.array(b))
 
-    def traverse(self):
-        if len(self.to_visit) > 0:
-            next_index = np.random.choice(range(len(self.to_visit)), 1, p=self.generate_roulette(self.path[-1], self.to_visit))[0]
-            next = self.to_visit[next_index]
-            self.to_visit.remove(next)
-            self.path.append(next)
-        if len(self.path) == len(self.points):
-            self.path.append(self.path[0])
+    def traverse(self, ant):
+        if len(self.to_visit[ant]) > 0:
+            next_index = np.random.choice(range(len(self.to_visit[ant])), 1, p=self.generate_roulette(self.path[ant][-1], self.to_visit[ant]))[0]
+            next = self.to_visit[ant][next_index]
+            self.to_visit[ant].remove(next)
+            self.path[ant].append(next)
+        if len(self.path[ant]) == len(self.points):
+            self.path[ant].append(self.path[ant][0])
+            distance = sum(map(lambda x: self.distance(x[0], x[1]), zip(self.path[ant], self.path[ant][1:])))
+            self.distances[ant] = distance
 
 
 def main():
@@ -70,14 +96,14 @@ def main():
 
     done = False
     clock = pygame.time.Clock()
-    aco = ACO(width, height)
+    aco = ACO(width, height, ants=15, points=5)
 
     while not done:
         done = aco.events()
         aco.run_logic()
         aco.display_screen(screen)
 
-        clock.tick(2)
+        clock.tick(5)
 
 
 if __name__ == "__main__":
